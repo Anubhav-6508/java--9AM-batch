1eabb648dd244f5b2ca66ee1d00ad62f4a90cd9c
Anubhav <rajanubhav495@gmail.com>
2026-09-11 17:22:13 +0530

    String

---
+++ core-java/NOTES/day11/String/StringDemo.java
@@ -0,0 +1,21 @@
+/*                         String 
+String sequence of character in double qoutes is called String.
+  "ksdjwbkrje" String 
+  
+  
+  String str="123"; */
+  
+  void main(){
+	  String str1="123";//1
+	  String str2="123";//1
+	  String str3=new String("123");  // 2
+	            //1010   ō1010
+	   IO.println(str1==str3);
+	    IO.println(str2);  
+	   String str4=""+9898;
+	     str2=str2+"ABCD";
+		
+	   IO.println(str2);
+	 
+	  
+  }
